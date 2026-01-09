bc77ee85b565197797889aa22a90fc0cb8c0c254
David Chu <dabidjohn424@gmail.com>
2026-01-09 21:44:11 +0800

    style: remove sidebar padding and add links transition

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,8 +9,6 @@ const Sidebar = ({ isOpen }) => {
     transform: isOpen ? "translateX(0)" : "translateX(-100%)",
     transition: "transform 0.3s ease",
     height: "100%",
-    paddingLeft: "2rem",
-    paddingRight: "2rem",
     backgroundColor: "white",
     position: "fixed",
     top: "0",
@@ -22,22 +20,34 @@ const Sidebar = ({ isOpen }) => {
   };
 
   return (
-    <div style={sidebarStyle} className="flex flex-col">
-      <p className="font-outfit text-3xl font-medium mt-5">MyStore</p>
-      <ul className="flex flex-col gap-5 text-sm mt-10 mr-10">
-        <Link className="flex items-center gap-5" to={"/"}>
+    <div style={sidebarStyle} className="flex flex-col px-5">
+      <p className="font-outfit text-3xl font-medium mt-5 pl-2 ">MyStore</p>
+      <ul className="flex flex-col text-sm mt-10">
+        <Link
+          className="flex items-center gap-5 p-2 rounded transition-all hover:bg-gray-200 active:bg-gray-300"
+          to={"/"}
+        >
           <HiOutlineHome size={30} />
           <p>Home</p>
         </Link>
-        <Link className="flex items-center gap-5" to={"categories"}>
+        <Link
+          className="flex items-center gap-5 p-2 rounded transition-all hover:bg-gray-200 active:bg-gray-300"
+          to={"categories"}
+        >
           <HiOutlineViewGrid size={30} />
           <p>Categories</p>
         </Link>
-        <Link className="flex items-center gap-5" to={"cart"}>
+        <Link
+          className="flex items-center gap-5 p-2 rounded transition-all hover:bg-gray-200 active:bg-gray-300"
+          to={"cart"}
+        >
           <HiOutlineShoppingCart size={30} />
           <p>Cart</p>
         </Link>
-        <Link className="flex items-center gap-5" to={"account"}>
+        <Link
+          className="flex items-center gap-5 p-2 rounded transition-all hover:bg-gray-200 active:bg-gray-300"
+          to={"account"}
+        >
           <HiOutlineUser size={30} />
           <p>Account</p>
         </Link>
