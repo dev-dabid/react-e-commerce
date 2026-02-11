394d98ee4362aced9e1436bd9bd8114ce7a73574
David Chu <dabidjohn424@gmail.com>
2026-02-11 11:01:20 +0800

    refactor: create component for delete modal, and remove handle confirm code linde from cart page

--- src/pages/Cart/CartPage.jsx
+++ src/pages/Cart/CartPage.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import useDeleteConfirm from "../../hooks/useDeleteConfirm";
 import CartItemCard from "../../components/CartItemCard";
 import CartTotal from "../../components/CartTotal";
+import DeleteModal from "../../components/DeleteModal";
 import { HiOutlineTrash } from "react-icons/hi";
 
 const Cart = () => {
@@ -24,9 +25,6 @@ const Cart = () => {
     if (targetId) {
       removeCartItem(targetId);
     }
-
-    console.log(`product: ${targetId} successfully deleted`);
-
     cancelDelete();
   };
 
@@ -36,30 +34,11 @@ const Cart = () => {
 
   return (
     <div className="">
-      {showConfirm && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
-          <div className="w-full max-w-[280px] md:max-w-[350px] rounded-lg bg-white p-6 shadow-lg">
-            <p className="mb-4 text-center text-lg font-medium">
-              Do you want to delete this product?
-            </p>
-
-            <div className="flex justify-end gap-3">
-              <button
-                onClick={cancelDelete}
-                className="rounded-md border px-4 py-2 text-sm hover:bg-gray-100"
-              >
-                Cancel
-              </button>
-
-              <button
-                onClick={handleConfirm}
-                className="rounded-md bg-gray-500 px-4 py-2 text-sm text-white hover:bg-gray-600"
-              >
-                Delete
-              </button>
-            </div>
-          </div>
-        </div>
+      {showConfirm === true && (
+        <DeleteModal
+          cancelDelete={cancelDelete}
+          handleConfirm={handleConfirm}
+        />
       )}
       {cart.length === 0 ? (
         <div className="flex justify-center items-center absolute w-full h-full">
