6c8a5adeef8751ffdf4c19eb395049cfc114e374
David Chu <dabidjohn424@gmail.com>
2026-02-12 21:57:35 +0800

    add: temporary account content

--- src/layouts/AccountLayout.jsx
+++ src/layouts/AccountLayout.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 const AccountLayout = () => {
   return (
     <main>
+      <Header />
+      <Sidebar />
       <Outlet />
     </main>
   );
